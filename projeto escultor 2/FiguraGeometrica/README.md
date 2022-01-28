a9e1ef18a184ad0389e886e2ef983c464cf97340
Vinicius <vinicius.henrique.018@ufrn.edu.br>
2022-01-27 23:05:18 -0300

    not delete idem

--- projeto escultor 2/FiguraGeometrica/README.md
+++
@@ -1,4 +0,0 @@
-@mainpage Voxel
-@author Sabrina E.S.A.
-
-### Programa em C++ que pega o texto digitado no arquivo usuario.txt e transforma ele em um arquivo 3D .off
--- projeto escultor 2/FiguraGeometrica/SOURCES/cutVoxel.cpp
+++
@@ -1,9 +0,0 @@
-#include "../HEADERS/cutVoxel.h"
-
-cutVoxel::cutVoxel(int x, int y, int z){
-    this->x=x; this->y=y; this->z=z;
-}
-
-void cutVoxel::draw(Sculptor &s){
-    s.cutVoxel(x,y,z);
-}
